2a96caa3a33ae2b2d7c69d430cf674ecb357432a
huyhoang462 <22520462@gm.uit.edu.vn>
2025-07-06 14:56:35 +0700

    fiix default port

--- src/server.js
+++ src/server.js
@@ -36,7 +36,7 @@ Database.getInstance();
 initWebRoutes(app);
 //
 
-let port = process.env.PORT || 6969;
+let port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log("Sever is running on port ", port);
